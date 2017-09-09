4f6546f717b4b5349841d3a6d87c6b329424abe9
Matthew Elmquist <mattelmquist@gmail.com>
2017-09-09 10:58:33 -0400

    about page layout

--- personal-website/src/components/About.jsx
+++ personal-website/src/components/About.jsx
@@ -13,10 +13,23 @@ export default class About extends React.Component{
         return (
             <div className="about-background-div">
                 <div className="about-content-div">
-                    <img className="about-image" src={aboutImg} alt="aboutImg" />
+                    <div className="about-image-div">
+                        <img className="about-image" src={aboutImg} alt="aboutImg" />
+                    </div>
 
-                </div>
+                    {/* 
+                    ADD TYPED.JS
+                    https://github.com/mattboldt/typed.js/
+                    
+                    TO REACT: 
+                    https://jsfiddle.net/mattboldt/ovat9jmp/
+                     */}
+                    <div className="about-header-div ralewayBold">Hi! My name is Matt Elmquist and I'm a Javascript Engineer</div>
+                    <div className="about-text-div ralewayRegular">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</div>
+                    </div>
             </div>
+
+            
         );
     }
 }
