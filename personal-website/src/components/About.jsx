a5c4d78f3fbe3a7c8808a31ae2f82194a260e6b8
Matthew Elmquist <mattelmquist@gmail.com>
2017-09-10 10:15:24 -0400

    initial typed.js working

--- personal-website/src/components/About.jsx
+++ personal-website/src/components/About.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import '../assets/main/main.css';
+import Typed from 'typed.js';
+
 
 const aboutImg = require('../assets/webImages/aboutImage.png');
 
@@ -7,7 +9,24 @@ export default class About extends React.Component{
     constructor(props) {
         super(props);
         this.state = {};
-    }1
+    }
+
+    componentDidMount() {
+        let strings = ['Hi my name is Matt Elmquist and I\'m a Fullstack Javascript Engineer']
+
+        // You can pass other options here, such as typing speed, back speed, etc.
+        const options = {
+    	    strings: strings,
+            typeSpeed: 50,
+        };
+
+        this.typed = new Typed(this.el, options);
+
+    }
+
+    componentWillUnmount() {
+        this.typed.destroy();
+    }
 
     render() {
         return (
@@ -24,7 +43,12 @@ export default class About extends React.Component{
                     TO REACT: 
                     https://jsfiddle.net/mattboldt/ovat9jmp/
                      */}
-                    <div className="about-header-div ralewayBold">Hi! My name is Matt Elmquist and I'm a Javascript Engineer</div>
+                    <div className="about-header-div ralewayBold">
+                        <span 
+                            style={{ whiteSpace: 'pre' }}
+                            ref={(el) => { this.el = el }}
+                        />
+                    </div>
                     <div className="about-text-div ralewayRegular">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</div>
                     </div>
             </div>
