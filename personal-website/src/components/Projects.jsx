d32a4eadfb6fab9f2d8a3d980ef02bf129665854
Matthew Elmquist <mattelmquist@gmail.com>
2017-09-13 17:24:03 -0400

    initial css animations but flicker bug now

--- personal-website/src/components/Projects.jsx
+++ personal-website/src/components/Projects.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import '../assets/main/main.css';
 import { Row, Col } from 'react-flexbox-grid';
 
+
 const autoiLogo = require('../assets/webImages/logo@3x.png');
 const autoiLogo2 = require('../assets/webImages/autoilogo.png');
 const wejayLogo = require('../assets/webImages/wejayLogo.png');
@@ -12,7 +13,7 @@ export default class Projects extends React.Component{
     constructor(props) {
         super(props);
         this.state = {};
-    }1
+    };
 
     render() {
         return (
@@ -21,21 +22,33 @@ export default class Projects extends React.Component{
                     <Row>   
                        
                         <Col className="" md={6} sm={6} xs={12}>
-                            <ProjectCard image={autoiLogo2}/>
+                            <ProjectCard 
+                                image={autoiLogo2}
+                                header="autoi"
+                            />
                             
                         </Col>
                         <Col className="" md={6} sm={6} xs={12}>
-                            <ProjectCard image={wejayLogo}/>
+                            <ProjectCard 
+                                image={wejayLogo}
+                                header="wejay"
+                            />
 
                         </Col> 
                     </Row> 
                     <Row> 
                         <Col className="" md={6} sm={6} xs={12}>
-                            <ProjectCard image={stackfighterImage}/>
+                            <ProjectCard 
+                                image={stackfighterImage}
+                                header="stackfighter"
+                            />
         
                         </Col>
                         <Col className="" md={6} sm={6} xs={12}>
-                            <ProjectCard image={journalistImage}/>
+                            <ProjectCard 
+                                image={journalistImage}
+                                header="journalist"
+                            />
            
                         </Col>
                          
@@ -50,18 +63,62 @@ export default class Projects extends React.Component{
     }
 }
 
-const ProjectCard = (props) => {
-    let { backColor, image} = props;
-    return (
-        <div className="projectCardOuter">
-            <div className="projectCard">   
-                <img className="projectImage" src={image} alt={image} />
-            </div>
-            <div className="animationHoverDiv">
+class ProjectCard extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            animationClasses: ['animationHoverDiv']
+        };
+        
+        // let animationClasses = ['animationHoverDiv'];
 
+        this.handleMouseEnter = this.handleMouseEnter.bind(this)
+        this.handleMouseLeave = this.handleMouseLeave.bind(this)
+    };
+
+    // let { backColor, image, header} = this.props;
+
+    handleMouseEnter() {
+        console.log("ENTER on " + this.props.header);
+        this.setState({
+            animationClasses: [...this.state.animationClasses, 'animationHoverDivEngaged']
+        })
+    }
+
+    handleMouseLeave() {
+        console.log("LEAVE on " + this.props.header);
+        // this.setState({
+        //     animationClasses: [...this.state.animationClasses, 'animationHoverDivEngaged']
+        // })
+        this.setState((prevState) => ({
+            animationClasses: [...prevState.animationClasses.slice(0,1), ...prevState.animationClasses.slice(1+1)]
+        }))
+    }
+
+    // function removeItem(index) {
+    //     this.setState((prevState) => ({
+    //         data: [...prevState.data.slice(0,index), ...prevState.data.slice(index+1)]
+    //     })
+    // }
+
+    
+    render() {
+        console.log("STATE PROJECT CARD : ", this.state)
+        return (
+            <div className="projectCardOuter" 
+                onMouseEnter={this.handleMouseEnter} 
+                onMouseLeave={this.handleMouseLeave}
+            >
+                <div className="projectCard">   
+                    <img className="projectImage" src={this.props.image} alt={this.props.image} />
+                </div>
+                <div className={this.state.animationClasses.join(' ')}>
+    
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
 }
 
 // const HoverTransition = () => {
