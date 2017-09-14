3551c6b5722ef6051119c8f4ce76f0f8309d04ab
Matthew Elmquist <mattelmquist@gmail.com>
2017-09-14 13:02:45 -0400

    transition group test

--- personal-website/src/components/Projects.jsx
+++ personal-website/src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import '../assets/main/main.css';
 import { Row, Col } from 'react-flexbox-grid';
+import Transition from 'react-transition-group/Transition';
 
 
 const autoiLogo = require('../assets/webImages/logo@3x.png');
@@ -67,39 +68,128 @@ class ProjectCard extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
+            // animationClasses: ['animationHoverDiv']
+            transitionInOut: false
+        };
+        
+        this.handleMouseEnter = this.handleMouseEnter.bind(this)
+        this.handleMouseLeave = this.handleMouseLeave.bind(this)
+        
+        const defaultStyle = {
+            transition: 'width 0.5s',
+            
+          }
+        const transitionStyles = {
+            entering: {
+                width: '85%'
+            },
+            entered: {
+                width: '85%'
+            },
+            exiting: {
+                width: '0%'
+            },
+            exited: {
+                width: '0%'
+            }
+        }
+       
+    };
+
+
+    handleMouseEnter() {
+        this.setState({
+            // animationClasses: [...this.state.animationClasses, 'animationHoverDivEngaged']
+            transitionInOut: true
+        })
+    }
+
+    handleMouseLeave() {
+        this.setState((prevState) => ({
+            // animationClasses: [...prevState.animationClasses.slice(0,1), ...prevState.animationClasses.slice(1+1)]
+            transitionInOut: false
+        }))
+    }
+
+    
+    render() {
+        console.log("STATE PROJECT CARD : ", this.state)
+        return (
+            <div className="projectCardOuter" 
+                onMouseEnter={this.handleMouseEnter} 
+                onMouseLeave={this.handleMouseLeave}
+            >
+                <div className="projectCard">   
+                    <img className="projectImage" src={this.props.image} alt={this.props.image} />
+                </div>
+                <Transition in={this.state.transitionInOut} timeout={500}>
+                    <div className="animationHoverDiv">
+                        ????animationHoverDiv???
+                    </div>
+                </Transition>
+            </div>
+        );
+    }
+    
+}
+
+
+
+// const HoverTransition = () => {
+//     const componentClasses = ['animationHoverDiv'];
+
+//     if(show)
+// }
+
+/*
+just one background div with picture, 
+on hover animate slides over and shows you links/options
+always with a *details* choice that will open up a fullscreen/greyed out dialog box
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ProjectCard extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
             animationClasses: ['animationHoverDiv']
         };
         
-        // let animationClasses = ['animationHoverDiv'];
-
         this.handleMouseEnter = this.handleMouseEnter.bind(this)
         this.handleMouseLeave = this.handleMouseLeave.bind(this)
     };
 
-    // let { backColor, image, header} = this.props;
 
     handleMouseEnter() {
-        console.log("ENTER on " + this.props.header);
         this.setState({
             animationClasses: [...this.state.animationClasses, 'animationHoverDivEngaged']
         })
     }
 
     handleMouseLeave() {
-        console.log("LEAVE on " + this.props.header);
-        // this.setState({
-        //     animationClasses: [...this.state.animationClasses, 'animationHoverDivEngaged']
-        // })
         this.setState((prevState) => ({
             animationClasses: [...prevState.animationClasses.slice(0,1), ...prevState.animationClasses.slice(1+1)]
         }))
     }
-
-    // function removeItem(index) {
-    //     this.setState((prevState) => ({
-    //         data: [...prevState.data.slice(0,index), ...prevState.data.slice(index+1)]
-    //     })
-    // }
 
     
     render() {
@@ -121,15 +211,10 @@ class ProjectCard extends React.Component{
     
 }
 
-// const HoverTransition = () => {
-//     const componentClasses = ['animationHoverDiv'];
 
-//     if(show)
-// }
 
-/*
-just one background div with picture, 
-on hover animate slides over and shows you links/options
-always with a *details* choice that will open up a fullscreen/greyed out dialog box
+
+
+
 
 */
