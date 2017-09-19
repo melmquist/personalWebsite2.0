20f6e067b37881674e2c7472e9715d4566985727
Matthew Elmquist <mattelmquist@gmail.com>
2017-09-19 14:29:28 -0400

    initial menu highlight

--- personal-website/src/components/Menu.jsx
+++ personal-website/src/components/Menu.jsx
@@ -5,7 +5,10 @@ export default class Menu extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            selected: 0
+            aboutSelected: true, 
+            projectsSelected: false, 
+            passionsSelected: false,
+            contactSelected: false
         };
         this.handleMenuClick = this.handleMenuClick.bind(this)
     }1
@@ -13,6 +16,52 @@ export default class Menu extends React.Component{
     handleMenuClick(val) {
         // this.setState({selected: val})
         this.props.snapScroll(val)
+        
+        switch(val) {
+            case 0: 
+                this.setState({
+                    aboutSelected: true, 
+                    projectsSelected: false, 
+                    passionsSelected: false,
+                    contactSelected: false
+                })
+                break;
+            
+            case 1: 
+                this.setState({
+                    aboutSelected: false, 
+                    projectsSelected: true, 
+                    passionsSelected: false,
+                    contactSelected: false
+                })
+                break;
+                
+            case 2: 
+                this.setState({
+                    aboutSelected: false, 
+                    projectsSelected: false, 
+                    passionsSelected: true,
+                    contactSelected: false
+                })
+                break;
+
+            case 3: 
+                this.setState({
+                    aboutSelected: false, 
+                    projectsSelected: false, 
+                    passionsSelected: false,
+                    contactSelected: true
+                })
+                break;
+
+            default:
+                this.setState({
+                    aboutSelected: true, 
+                    projectsSelected: false, 
+                    passionsSelected: false,
+                    contactSelected: false
+                })
+        }
 
     }
 
@@ -22,10 +71,10 @@ export default class Menu extends React.Component{
             <div className="menu-background-div ">
                 <div className="vertical-line "></div>
                 <div className="menu-content-div ">    
-                    <div className="about menu-item playfairRegular" onClick={() => this.handleMenuClick(0)}>About</div>
-                    <div className="projects menu-item playfairRegular" onClick={() => this.handleMenuClick(1)}>Projects</div>
-                    <div className="passions menu-item playfairRegular" onClick={() => this.handleMenuClick(2)}>Passions</div>
-                    <div className="contact menu-item playfairRegular" onClick={() => this.handleMenuClick(3)}>Contact</div>
+                    <div className={`about menu-item playfairRegular about-selected-${this.state.aboutSelected}`} onClick={() => this.handleMenuClick(0)}>About</div>
+                    <div className={`projects menu-item playfairRegular projects-selected-${this.state.projectsSelected}`} onClick={() => this.handleMenuClick(1)}>Projects</div>
+                    <div className={`passions menu-item playfairRegular passions-selected-${this.state.passionsSelected}`} onClick={() => this.handleMenuClick(2)}>Passions</div>
+                    <div className={`contact menu-item playfairRegular contact-selected-${this.state.contactSelected}`} onClick={() => this.handleMenuClick(3)}>Contact</div>
                 </div>
             </div>
         );
