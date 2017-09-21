55d6783387a34862185a86d09cd40fede82b0bf8
Matthew Elmquist <mattelmquist@gmail.com>
2017-09-20 20:45:20 -0400

    three photos layout

--- personal-website/src/components/Passions.jsx
+++ personal-website/src/components/Passions.jsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import '../assets/main/main.css';
 
+const music = require("../assets/webImages/music.JPG");
+const food = require("../assets/webImages/food.JPG");
+const soccer = require("../assets/webImages/soccer.JPG");
+
 export default class Passions extends React.Component{
     constructor(props) {
         super(props);
@@ -17,32 +21,52 @@ export default class Passions extends React.Component{
                     </div>
 
 
-                    <div className="passion-div">
-                        <div className="passion-header ralewayBold">
-                            Soccer
+                    <div className="passion-div ">
+                        <div className="passion-img-div">    
+                            <img src={soccer} alt="imagasddase" />
                         </div>
-                        <div className="passion-copy ralewayRegular">
-                            On any given weekend, you can probably find me on a pitch somewhere in Brooklyn playing pickup soccer. To me, this sport is more than just a sport. It is a vehicle for reminding ourselves that while we may come from different cultures, we are all essentially the same. 
+                        <div className="passion-text-div">   
+
+                            <div className="passion-header ralewayBold">
+                                Soccer
+                            </div>
+                            <div className="passion-copy ralewayRegular">
+                                On any given weekend, you can probably find me on a pitch somewhere in Brooklyn playing pickup soccer. To me, this sport is more than just a sport. It is a vehicle for reminding ourselves that while we may come from different cultures, we are all essentially the same. 
+                            </div>
                         </div>
                     </div>
 
 
-                    <div className="passion-div">
-                        <div className="passion-header ralewayBold">
-                            Food
+                    <div className="passion-div ">
+                        
+
+                        <div className="passion-text-div">   
+                            <div className="passion-header ralewayBold">
+                                Food
+                            </div>
+                            <div className="passion-copy ralewayRegular">
+                                I love to cook almost as much as I love to eat and entertain. Eating well and occasionally wowing my friends is great but recently I have become very interested in the American food system and in the long term I hope to work on projects that improve our society’s access to quality food from small scale producers.
+                            </div>
                         </div>
-                        <div className="passion-copy ralewayRegular">
-                            I love to cook almost as much as I love to eat and entertain. Eating well and occasionally wowing my friends is great but recently I have become very interested in the American food system and in the long term I hope to work on projects that improve our society’s access to quality food from small scale producers.
+                        <div className="passion-img-div">    
+                            <img src={food} alt="imagasddase" />
                         </div>
+                        
                     </div>
 
 
-                    <div className="passion-div">
-                        <div className="passion-header ralewayBold">
-                            Music
+                    <div className="passion-div ">
+                        <div className="passion-img-div">    
+                            <img src={music} alt="imagasddase" />
                         </div>
-                        <div className="passion-copy ralewayRegular ">
-                            I grew up in NYC and played in bands throughout my life...I still play but now I like to listen and appreciate more about different types of music because like all art, it tells you about the culture and subject matter of where it came from
+
+                        <div className="passion-text-div">   
+                            <div className="passion-header ralewayBold">
+                                Music
+                            </div>
+                            <div className="passion-copy ralewayRegular ">
+                                I grew up in NYC and played in bands throughout my life...I still play but now I like to listen and appreciate more about different types of music because like all art, it tells you about the culture and subject matter of where it came from
+                            </div>
                         </div>
                     </div>
 
